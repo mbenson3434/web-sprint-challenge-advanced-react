05132503a8b3a37ec3a6d57f0e72ff7a75179e8d
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-30 11:42:33 -0400

    useForm working

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from "react";
+
+const useForm = (initialValue1, initialValue2) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(initialValue2);
+    const [values, setValues] = useState(initialValue1);
+    
+    const handleChanges = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+  
+    return([values, handleChanges, showSuccessMessage, handleSubmit]);
+  }
+
+  export default useForm;
